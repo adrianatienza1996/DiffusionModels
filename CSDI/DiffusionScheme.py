174ee77f695f61511fc0e90698e4e25e7ef73ba0
Adrian Atienza <adrian.atienza.1996@gmail.com>
2022-11-25 14:00:22 +0100

    Corrections

--- CSDI/DiffusionScheme.py
+++ CSDI/DiffusionScheme.py
@@ -1,5 +1,6 @@
 import torch
 import math
+import numpy as np
 
 class Diffusion:
     
@@ -25,12 +26,27 @@ class Diffusion:
         beta_schedule = [cuadratic_scheme(self, t) for t in range(self.noise_steps)]
         return torch.tensor(beta_schedule)
 
-    def noise_signal(self, x, t):
+    def noise_signal(self, x):
+        # x is (4 x fs * l) dim . (ECG, BP, EEG, Resp)
+         
+        def sample_t_and_mask(self):
+            t = np.random.randint(1, self.noise_steps)
+
+            # Hidden percentaje between (10%, 90%) of the ECG Signal
+            first_index = np.random.randint(0, int(np.round(0.9 * self.fs * self.l)))
+            second_index = np.random.randint(first_index + int(np.round(0.1 * self.fs * self.l)), int(self.fs * self.l))
+
+            mask = np.zeros_like(x)
+            mask[0, first_index:second_index] = 1
+
+            return t, mask
+
+        t, mask = sample_t_and_mask(self)
+
         sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None]
         sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None, None]
-        Ɛ = torch.randn_like(x)
         
-        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * Ɛ, Ɛ
+        Ɛ = torch.randn_like(x) * mask
+        
+        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * Ɛ, Ɛ, mask
 
-    def sample_timesteps(self):
-        return torch.randint(low=1, high=self.noise_steps)
